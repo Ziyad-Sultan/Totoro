27af9d1034bdd669240dd2796b7b29c9cec0acf5
Ziyad-Sultan <166725066+Ziyad-Sultan@users.noreply.github.com>
2024-06-22 07:44:40 +0300

    Create Plugin.cs

---
+++ Plugins/Anime/Totoro.Plugins.Anime.Anime4Up/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Totoro.Plugins.Anime.Contracts;
+using Totoro.Plugins.Contracts;
+
+namespace Totoro.Plugins.Anime.Anime4Up;
+
+public class Plugin : Plugin<AnimeProvider, Config>
+{
+    public override AnimeProvider Create() => new()
+    {
+        Catalog = new Catalog(),
+        StreamProvider = new StreamProvider(),
+        AiredAnimeEpisodeProvider = new AiredEpisodesProvider(),
+        IdMapper = new IdMapper(),
+    };
+
+    public override PluginInfo GetInfo() => new()
+    {
+        DisplayName = "Anime4Up",
+        Name = "anime4up",
+        Version = Assembly.GetExecutingAssembly().GetName().Version!,
+        Icon = typeof(Plugin).Assembly.GetManifestResourceStream("Totoro.Plugins.Anime.Anime4Up.anime4up-logo.png"),
+        Description = "Arabic Provider - Made By Ziyad Sultan(Meka Beam on Youtube"
+    };
+}
